43b4345f876b208a7c3443b67a96b694049f643c
Joel Warburton <joel.s.warburton@gmail.com>
2022-05-07 16:07:43 +1000

    remove generated files.

--- src/main/java/com/joelwarburton/todo/gui/UI.java
+++ src/main/java/com/joelwarburton/todo/gui/UI.java
@@ -32,23 +32,29 @@ public class UI extends Application {
 
     @Override
     public void start(Stage window) {
-
+        /*
+        * The main view should have two sub-views by default.
+        * the list pane on the left which houses all the lists and functions to add or remove lists
+        * on the right is the selected list which contains the tasks in that list.
+        * */
 
         //Build the main layout
         BorderPane mainLayout = new BorderPane();
         mainLayout.setPrefSize(500, 500);
 
-
+        /*Set up the list pane for display*/
         listPane = new ListPane(this);
         listPane.setPrefWidth(150);
         mainLayout.setLeft(listPane);
+
+        /*display the selected list. If a list does not exist display a placeholder */
         System.out.println(lists);
         if (listPane.getListItem(0) == null) {
             TodoView todoView = new TodoView();
             mainLayout.setCenter(todoView);
 
-            // Label empty = new Label("There are no lists. Create one!");
-            //mainLayout.setCenter(empty);
+             Label empty = new Label("There are no lists. Create one!");
+             mainLayout.setCenter(empty);
         } else {
             TodoView todoView = new TodoView();
             mainLayout.setCenter(todoView);
